cd5aedb83c67c238f5174026efcf19fdd39aded7
flash <mymail@codenaschereien.de>
2011-10-04 22:45:29 +0200

    Initial commit. Use strings statically.

---
+++ StaticString.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once('String.php');
+
+/**
+ * @author Martin Will
+ */
+class StaticString {
+  
+  /**
+   * Create a new String object and call a method on it
+   * @param string $name
+   * @param array $arguments
+   * @return String 
+   */
+  public static function __callStatic($name, $arguments) {
+    return call_user_func_array(array(String::factory(array_shift($arguments)), $name), $arguments)
+      ->getValue();
+  }
+}
